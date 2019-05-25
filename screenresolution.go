58c6e2659cd550bcba995165de3432e393fff8da
Filip Stanis <fstanis@google.com>
2019-05-25 23:44:59 +0100

    Add String method

--- screenresolution.go
+++ screenresolution.go
@@ -1,9 +1,18 @@
 // Package screenresolution is used to retrieve the current screen resolution.
 package screenresolution
 
+import "fmt"
+
 // Resolution represents the resolution of a single screen.
 type Resolution struct {
 	Width, Height int
+}
+
+func (r *Resolution) String() string {
+	if r == nil {
+		return ""
+	}
+	return fmt.Sprintf("%dx%d", r.Width, r.Height)
 }
 
 // Get returns the current screen resolution of the primary display.
